aa7091c606c7fa790f822ef6532fca08dcb16b98
Dani Long <46729930+Dani-Long@users.noreply.github.com>
2021-10-12 10:38:47 -0500

    Update FBG_average.py

--- FBG_average.py
+++ FBG_average.py
@@ -1,14 +1,20 @@
 '''
-"Patient_FBG_2021.csv"
-has FBG (fasting blood glucose) values
+NOTE: there are several 'print(data)' lines in the code.
+      These are there to help the student I was tutoring, 
+      so they would learn how to check their code as they go.
+      I would triple-quote remaining code and just run up to
+      the print line, so progress can be monitored (if print
+      returns what is expected, carry on. If not, fix something).
+      
+      Feel free to delete those lines if you like.
+
+Instructions for assignment:
+"Patient_FBG_2021.csv" has FBG (fasting blood glucose) values 
 for 200 subjects across three years.
 
-Write a program that finds
-the average FBG for each year.
-Your program should read in the file
-and write out the answer to a ".txt".
-You will also need to include at least
-one function within your script.
+Write a program that finds the average FBG for each year.
+Your program should read in the file and write out the answer to a ".txt".
+You will also need to include at least one function within your script.
 
 Your output should look like this
 (note that these are not the actual answers):
@@ -17,56 +23,54 @@ Average FBG for Year_1:    121
 Average FBG for Year_2:    130
 Average FBG for Year_3:    107
 
-Please make sure to submit both your script
-and the output .txt file to brightspace.
-
-
 '''
+# Remember to update your paths for your local environment
+csvpath = '/add/path/to/csv/'
+txtpath = '/file/will/be/created/here'
 
 def get_data(csvpath):
-    # Read in and tidy up the csv:
+    # Read in the csv:
     with open(csvpath, 'r') as f:
-        # read from csv line by line
-        # and rstrip (right end strip) to remove '\n' at the end of line
+        # Read from csv line by line
+        # and rstrip (right-end strip) to remove '\n' at the end of line
         lines = [line.rstrip() for line in f]
-        # print(lines)
-        data = [line.split(',') for line in lines]
         # print(data)
         return data
 
-def clean_up(data):
-    # Remove the header row from data list:
+def clean_up(data):  
+    # Data is a list of strings, so split into a list of lists
+    data = [line.split(',') for line in lines]
+    # print(data)
+    
+    # Remove the header row from data list (first element in LoL)
     data.pop(0)
 
-    # Remove the first column (names):
+    # Remove the first column (names) from LoL
     for element in data:
         element.pop(0)
     # print(data)
 
-    # Convert strings to ints:
+    # Convert strings to ints so we can get averages
     data = [[int(item) for item in element] for element in data]
-    # print(data)
+    # print(data)  # Should be a list of lists, each element has 3 ints
     return data
 
 def do_maths(data):
-    # Calculate averages, round and convert to int:
+    # Calculate averages, round and convert resulting float to int:
     # From stackoverflow, shorturl.at/jtJNV
     averages = [sum(x)/len(x) for x in zip(*data)]
     averages = [int(element) for element in averages]
-    # print(averages)
+    # print(averages)    # Should be a list of 3 integers
     return averages
    
 def write_file(averages, txtpath):
     i = 0
-    with open(txtpath, 'a+') as f:  # a+ for append/don't want to overwrite
+    with open(txtpath, 'a+') as f:  # a+ for append b/c don't want to overwrite
         for i in range(3):
             output = 'Average FBG for Year_{}:\t{}\n'.format(i, averages[i])
-            # print(output)
+            # print(output)  # Will show you what's writing to .txt file
             f.write(output)
             i += 1
-
-csvpath = '/home/dani/Documents/Patient_FBG_2021.csv'
-txtpath = '/home/dani/Documents/averages.txt'
 
 data = get_data(csvpath)
 data = clean_up(data)
